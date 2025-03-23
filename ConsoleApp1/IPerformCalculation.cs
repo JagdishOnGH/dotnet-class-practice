f83a21ec79a52c3575c204fc12822b9ce8893fcd
jagdish bhai <jagdissghimire@gmail.com>
2025-03-23 20:51:53 +0545

    feat: calc added

--- ConsoleApp1/IPerformCalculation.cs
+++ ConsoleApp1/IPerformCalculation.cs
@@ -1,6 +1,46 @@
 ﻿namespace ConsoleApp1;
 
-public interface IPerformCalculation
+public interface IPerformCalculation 
+
 {
+   public   decimal   PerformCalculation(decimal a, decimal b);
     
 }
+
+class Addition : IPerformCalculation
+{
+   public decimal PerformCalculation(decimal a, decimal b)
+   {
+         return a + b;
+      
+   }
+}
+
+//same for subtraction, multiplication and division
+class Subtraction : IPerformCalculation
+{
+   public decimal PerformCalculation(decimal a, decimal b)
+   {
+         return a - b;
+      
+   }
+}
+
+class Multiplication : IPerformCalculation
+{
+   public decimal PerformCalculation(decimal a, decimal b)
+   {
+         return a * b;
+      
+   }
+}
+
+class Division : IPerformCalculation
+{
+   public decimal PerformCalculation(decimal a, decimal b)
+   {
+         return a / b;
+      
+   }
+}
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
+var continueLoop = true;
+
+decimal f1 = 10;
+decimal f2 = 20;
+while (continueLoop)
+    
+{
+    Console.WriteLine("Enter an operation (+, -, *, /) or type 'exit' to quit");
+    var input = Console.ReadLine();
+    if (input != null)
+    {
+        //if input contains any of the following characters, it will return the corresponding operation (+, -, *, /)
+        if(input == "+" || input == "-" || input == "*" || input == "/")
+        {
+            IPerformCalculation calculation = input switch
+            {
+                "+" => new Addition(),
+                "-" => new Subtraction(),
+                "*" => new Multiplication(),
+                "/" => new Division(),
+                _ => throw new InvalidOperationException("Invalid operation")
+            };
+            Console.WriteLine(calculation.PerformCalculation(f1, f2));
+        }
+        else if(input == "exit")
+        {
+            Environment.Exit(0);
+            continueLoop = false;
+        }
+        else
+        {
+            Console.WriteLine("Invalid operation");
+        }
+    
+    
+    }
+    
+}
+
